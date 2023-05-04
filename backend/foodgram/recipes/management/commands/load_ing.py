8aee9e69418d14a6bace9db7808df7cb739daabd
Максим <maksim@MacBook-Air-Exon.local>
2023-05-04 16:37:55 +0300

    all fix

--- backend/foodgram/recipes/management/commands/load_ing.py
+++ backend/foodgram/recipes/management/commands/load_ing.py
@@ -9,7 +9,7 @@ from recipes.models import Ingredient
 class Command(BaseCommand):
     def handle(self, *args, **options):
 
-        with open("/Users/maksim/Desktop/!/dev/foodgram-project-react/data/ingredients.json", "rb") as f:
+        with open("/app/data/ingredients.json", "rb") as f:
             data = json.load(f)
 
             for val in data:
