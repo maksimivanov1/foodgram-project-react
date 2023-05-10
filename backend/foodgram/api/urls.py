a504e543f0a322148494451b267b6e586bd2b297
Максим <maksim@MacBook-Air-Exon.local>
2023-05-10 14:45:08 +0300

    fixx

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api.views import (AddAndDeleteSubscribe, AddDeleteFavoriteRecipe,
-                       AddDeleteShoppingCart, AuthToken, IngredientsViewSet,
+                       AuthToken, IngredientsViewSet,
                        RecipesViewSet, TagsViewSet, UsersViewSet, set_password)
 
 app_name = 'api'
